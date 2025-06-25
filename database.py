786dedbc24d2ffbb487aa5d3e89f591572cef0ea
Pranav Kumar Pandey <pranavkumar.pandey@buzzybrains.com>
2025-06-25 16:26:44 +0530

    changes

--- database.py
+++ database.py
@@ -18,8 +18,12 @@ encoded_password = urllib.parse.quote(DB_PASSWORD)
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{encoded_password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+print("Before printing BD URL")
 print(SQLALCHEMY_DATABASE_URL)
+print("After printing BD URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 Base = declarative_base()
